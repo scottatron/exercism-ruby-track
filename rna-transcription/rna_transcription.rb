dbcf757e435d1ad71ca5199d1123a99b95eedee2
Scott Arthur <scott@scottatron.com>
2019-02-23 10:56:35 +1100

    Simply RNA Transcription with `String#tr`

--- rna-transcription/rna_transcription.rb
+++ rna-transcription/rna_transcription.rb
@@ -1,14 +1,5 @@
 class Complement
   def self.of_dna(dna_strand)
-    DNA_TO_RNA.values_at(*dna_strand.chars).join
+    dna_strand.tr "GCTA", "CGAU"
   end
-
-  private
-
-  DNA_TO_RNA = {
-    "G" => "C",
-    "C" => "G",
-    "T" => "A",
-    "A" => "U"
-  }.freeze
 end
