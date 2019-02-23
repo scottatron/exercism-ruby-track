f2141771986c359f91a1a4945044f46032643123
Scott Arthur <scott@scottatron.com>
2019-02-23 11:05:31 +1100

    Use `define_method` instead of `method_missing` for Space Age

--- space-age/space_age.rb
+++ space-age/space_age.rb
@@ -7,15 +7,7 @@ class SpaceAge
     @seconds / 31557600.to_f
   end
 
-  def method_missing(method_name)
-    planet = method_name.to_s.sub(/on_/, "")
-    super unless EARTH_DELTAS.keys.include?(planet)
-    on_earth / EARTH_DELTAS.fetch(planet)
-  end
-
-  private
-
-  EARTH_DELTAS = {
+  {
     "mercury" => 0.2408467,
     "venus"   => 0.61519726,
     "mars"    => 1.8808158,
@@ -23,5 +15,9 @@ class SpaceAge
     "saturn"  => 29.447498,
     "uranus"  => 84.016846,
     "neptune" => 164.79132
-  }.freeze
+  }.each do |planet, delta|
+    define_method "on_#{planet}" do
+      on_earth / delta
+    end
+  end
 end
