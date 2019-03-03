f8d2ccbba594b7c7aba0c07fae3f843bad83fe44
Scott Arthur <scott@scottatron.com>
2019-03-03 13:01:04 +1100

    Use `String#lines` instead of `#split` in Matrix

--- matrix/matrix.rb
+++ matrix/matrix.rb
@@ -2,7 +2,7 @@ class Matrix
   attr_reader :rows
 
   def initialize(string)
-    @rows = string.split("\n").map { |r| r.scan(/\d+/).map(&:to_i) }
+    @rows = string.lines.map { |r| r.scan(/\d+/).map(&:to_i) }
   end
 
   def columns
